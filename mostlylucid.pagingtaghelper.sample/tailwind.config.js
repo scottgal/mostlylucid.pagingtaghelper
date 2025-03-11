ca6f0445aa62b715a219d522bbd3844385573e6f
Scott Galloway <scott.galloway@zenchef.com>
2025-03-11 19:44:18 +0000

    Layout fixes.

--- mostlylucid.pagingtaghelper.sample/tailwind.config.js
+++ mostlylucid.pagingtaghelper.sample/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./Views/**/*.cshtml"],
+  safelist: ["dark", "light"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
